2545090c17e66f6550654809e9423ef1fcaa7023
Fleur Van Buiijten <fleurvanbuijten@gmail.com>
2022-12-17 03:00:26 +0100

    use IGameObject to draw and update

--- MyGame/Game1.cs
+++ MyGame/Game1.cs
@@ -2,45 +2,50 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MyGame.Input;
-using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Content;
 using MyGame.Character;
+using MyGame.Content.interfaces;
 
 namespace MyGame
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
         private SpriteBatch spriteBatch;
-        Hero hero;
-        
+        private List<IGameObject> gameObjects;
+
         public Game1()
-        {  
-            _graphics = new GraphicsDeviceManager(this);
+        {
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            
+            GraphicsDeviceManager graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
         }
         
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            
             InitializeGameObjects(Content);
         }
 
         private void InitializeGameObjects(ContentManager contentManager)
         {
-            hero = new Hero(contentManager, new KeyboardReader());
+            gameObjects = new List<IGameObject>
+            {
+                new Hero(contentManager, new KeyboardReader()),
+            };
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
-            // TODO: Add your update logic here
-            hero.Update(gameTime);
+            }
+
+            gameObjects.ForEach(gameObject => gameObject.Update(gameTime));
             base.Update(gameTime);
         }
 
@@ -48,14 +53,11 @@ namespace MyGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            //overload spriteBatch.Begin because of the hero scaling was blurry
             spriteBatch.Begin(SpriteSortMode.BackToFront,null, SamplerState.PointClamp,null,null);
-            hero.Draw(spriteBatch);
+            gameObjects.ForEach(gameObject => gameObject.Draw(spriteBatch));
             spriteBatch.End();
             
             base.Draw(gameTime);
         }
-
-
     }
 }
